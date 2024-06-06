3cd314fdbb9922dc9747dff65937110efef105ff
david gomes <davidgomesk8@gmail.com>
2024-06-06 21:55:41 +0200

    model tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :user
+
+  has_many :tasks, dependent: :destroy
 end
---
+++ db/migrate/20240606195506_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.date :start_date
+      t.date :end_date
+      t.references :project, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
