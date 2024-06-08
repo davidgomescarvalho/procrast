c31212c34aab7014911c873f33a149bd19d7dfeb
Wassim Badreddine <r00t@c0der.io>
2024-06-08 14:19:24 +0200

    chore: Update tasks controller with new actions and strong parameters

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,36 @@
 class TasksController < ApplicationController
 
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:notice] = 'Task was successfully created.'
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :project_id)
+  end
+
 end
