4be39b33668dcb22c2906447a9ba02cb5a3c29d9
Hugo Ferrier <hu.ferrier@gmail.com>
2024-06-08 12:51:51 +0200

    Add gem dependencies and migrations for user-related features

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,47 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to @project, notice: 'Project was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project, notice: 'Project was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_url, notice: 'Project was successfully destroyed.'
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
   has_many :projects, dependent: :destroy
   has_many :achievements, dependent: :destroy
   has_many :trophies, through: :achievements
