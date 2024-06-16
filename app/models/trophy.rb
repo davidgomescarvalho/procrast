bd128fff5db8d53a5766a16ba89ebbbcc69af065
Hugo Ferrier <hu.ferrier@gmail.com>
2024-06-16 02:23:57 +0200

    Update project and task models to update user points

--- app/models/trophy.rb
+++ app/models/trophy.rb
@@ -2,18 +2,8 @@ class Trophy < ApplicationRecord
   has_one_attached :image
   validates :points, presence: true
 
-  def user_points
-    case projects.count += 1
-    when 1
-      user.total_points += 10
-    when 5
-      user.points += 20
-    when 10
-      user.points += 30
-    else
-    end
-  end
 
   def grant_trophy
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,6 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: "users/registrations"
   }
-  resources :notifications, only: [] do
-    member do
-      patch :mark_as_read
-    end
-  end
 
   resources :users, only: [:show, :index] do
     # resources :projects, only: [:index, :new, :create,:show]
@@ -19,8 +14,6 @@ Rails.application.routes.draw do
   end
 
   resources :tasks, only: [:show, :edit, :update, :destroy]
-
-  resources :achievements, only: [:index, :show ]
 
   resources :trophies, only: [:show, :update, :index]
 
