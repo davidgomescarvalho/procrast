775bb51c49f1ff7eaf034df42045e4e49adbd4fc
Hugo Ferrier <hu.ferrier@gmail.com>
2024-06-20 19:58:07 +0200

    chore: Add recent trophies to user show page

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
-import rangePlugin from "flatpickrRangePlugin";
+// import rangePlugin from "flatpickrRangePlugin";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
@@ -8,14 +8,14 @@ export default class extends Controller {
   static values = { minDate: String, maxDate: String }
 
   connect() {
-    flatpickr(this.startDateTarget, {
-      altInput: true,
-      altFormat: "F j, Y",
-      dateFormat: "Y-m-d",
-      minDate: this.minDateValue,
-      maxDate: this.maxDateValue,
-      defaultDate: this.element.value,
-      "plugins": [new rangePlugin({ input: this.endDateTarget})]
-    })
+    flatpickr(this.element)
+      // altInput: true,
+      // altFormat: "F j, Y",
+      // dateFormat: "Y-m-d",
+      // minDate: this.minDateValue,
+      // maxDate: this.maxDateValue,
+      // defaultDate: this.element.value
+      // "plugins": [new rangePlugin({ input: this.endDateTarget})]
+    
   }
 }
